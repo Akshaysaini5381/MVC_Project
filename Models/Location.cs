48a55cf94c052c42f22b8c10d7ecc5e6e9fce58d
admin <admin@CHETUIWK385>
2022-04-12 18:35:31 +0530

    Add project files.

---
+++ Models/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Models
+{
+    public class Location
+    {
+
+
+        public int Id { get; set; }
+        public string Place { get; set; }
+        public string Detalis { get; set; }
+    }
+    
+}
